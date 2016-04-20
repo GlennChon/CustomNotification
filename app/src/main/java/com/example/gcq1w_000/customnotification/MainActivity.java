c4004142e3857f29fc100211f69361f62577839f
Glenn Chon <glenn.chon@gmail.com>
2016-04-20 14:43:36 -0400

    first commit

---
+++ app/src/main/java/com/example/gcq1w_000/customnotification/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.gcq1w_000.customnotification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.app.WallpaperInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 1, intent, 0);
+
+        Notification notification = new Notification.Builder(getApplicationContext())
+                .setContentTitle("@string/ContentTitle")
+                .setContentText("@string/ContentText")
+                .setContentIntent(pendingIntent)
+                .addAction(android.R.drawable.sym_action_chat, "Chat", pendingIntent)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(1, notification);
+        }
+
+}
+
+
+
